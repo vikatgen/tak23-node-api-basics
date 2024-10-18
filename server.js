f776d69fa4cb30fd60ecafa66d53ac7699bae3c7
Gen Vikat <gen@veebisepad.ee>
2024-10-18 11:42:11 +0300

    Added router and controller with in memory array to hande car resource

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from "express";
+import dotenv from "dotenv";
+import carRoutes from "./routes/carRoutes.js";
 
-dotenv.config()
+dotenv.config();
 
-const PORT = import.meta.PORT || 3006;
+const PORT = process.env.PORT || 3006;
 
-const app = express()
+const app = express();
+app.use(express.json());
 
 app.get("/", (request, response) => {
     response.json({
-        message: "hello from server"
-    })
-})
+        message: "hello from server",
+    });
+});
+
+app.use(carRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server listening on http://localhost:${PORT}`)
-})
+    console.log(`Server listening on http://localhost:${PORT}`);
+});
