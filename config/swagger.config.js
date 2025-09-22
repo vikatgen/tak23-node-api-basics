f2345d20e7049f94109c04152e25fcbcf7fd62a5
vikatgen <vikat.gen@gmail.com>
2025-09-22 09:55:34 +0300

    adds new swagger config file, using docs file instead routes

---
+++ config/swagger.config.js
@@ -0,0 +1,43 @@
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from "swagger-jsdoc";
+import bookDocs from "../docs/swagger/book.docs.js";
+import authDocs from "../docs/swagger/auth.docs.js";
+import schemas from "../docs/swagger/schema.js";
+import environment from "./config.js";
+
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'TAK23 Book REST API',
+            version: '1.0.0',
+            description: 'NodeJS Express backend API for book management.'
+        },
+        servers: [
+            {
+                url: `http://localhost:${environment.PORT}`,
+            },
+        ],
+        paths: {
+            ...bookDocs,
+            ...authDocs,
+        },
+        components: {
+            schemas: schemas,
+            securitySchemes: {
+                bearerAuth: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT',
+                },
+            },
+        },
+    },
+    apis: [],
+};
+
+const specs = swaggerJSDoc(swaggerOptions);
+
+export const setupSwagger = (app) => {
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+}
